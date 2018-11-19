29f9807f65cdcb34d4d46709c74db6b2f7ada394
khoursaaf <khoursa@gmail.com>
2018-11-19 10:09:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+      <p>Créer une variable age et l'initialiser avec une valeur.
+        Si l'âge est supérieur ou égale à 18, afficher Vous êtes majeur. Dans le cas contraire, afficher Vous êtes mineur.</p>
+        <?php
+        $age = 22;
+        if ($age >= 18) {
+          echo "Vous êtes majeur.";
+        } else {
+          echo "Vous êtes mineur.";
+        }
+        ?>
+    </body>
+</html>
